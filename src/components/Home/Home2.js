308ef98b23d4d99ee4600a0053374e57b4bcc28e
anotherreeguy <168342264+anotherreeguy@users.noreply.github.com>
2024-05-05 03:07:53 +0800

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,36 +19,36 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I started editing videos since Ive gotten a computer
+                
               <br />
-              <br />I am fluent in classics like
+              <br />I am fluent in
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> HitFilm, Premire Pro and Davinci Resolve. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+              I will edit any video you send for &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                <b className="purple">free </b> and
+                , I also own{" "}
                 <b className="purple">
-                  Blockchain.
+                  ReeBit.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              Whenever possible, I also apply my passion for video editing
+              with <b className="purple">scripting</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  and much much more.
                 </b>
               </i>
-              &nbsp; like
+              &nbsp; For eg,
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> Making custom Minecraft plugins for videos</b>
               </i>
             </p>
           </Col>
